3f1a2b5e79af9a893824f687ec1f9c6b5f0684a2
Avonexile <arnevanommen@outlook.com>
2018-02-08 19:02:05 +0100

    Main menu Testing

---
+++ Unity/Project Survival/Assets/Classes/Arne/Scripts/RotateCamera.cs
@@ -0,0 +1,14 @@
+﻿//Made by Arne
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotateCamera : MonoBehaviour {
+
+	//constantly rotates
+	void FixedUpdate () 
+	{
+		transform.Rotate(Vector3.up * Time.deltaTime);	
+	}
+}
