b4974a1cf629f5af67ad792ef0156c49997a5ade
Avonexile <arnevanommen@outlook.com>
2018-02-11 15:35:45 +0100

    Some progress on UIManager and UI setup

--- Unity/Project Survival/Assets/Classes/Arne/Scripts/UIManager.cs
+++ Unity/Project Survival/Assets/Classes/Arne/Scripts/UIManager.cs
@@ -1,24 +1,49 @@
-﻿using System.Collections;
+﻿//Made by Arne
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour 
 {
+	//health test related stuff
+	public float maxHP;
+	public float health;
+	private float healthPercentage;
+	public float dmg;
+	public Image healthBar;
 
-	// Use this for initialization
-	void Start () 
-	{
-		
+
+	public KeyCode key;
+
+
+	//sets some things ready
+	void Awake () 
+	{	
+		maxHP = 100;
+		health = maxHP;
+
 	}
-	
-	// Update is called once per frame
+	//for testing purposes
 	void Update () 
 	{
-		
+		if(Input.GetKeyDown(key))
+		{
+			health -= dmg;
+		}
+		CheckHealth();
 	}
 	//checks health and updates ui
 	public void CheckHealth ()
+	{
+		//get a percentage of health, because fillamount is between 1 and 0
+		healthPercentage = health / maxHP;
+		Debug.Log(healthPercentage);
+		healthBar.fillAmount = healthPercentage;
+		
+	}
+	//checks ammo and updates ui    	NOT MADE YET
+	public void CheckAmmo () 
 	{
 
 	}
