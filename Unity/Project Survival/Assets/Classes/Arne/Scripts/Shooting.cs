646e4fd2e78137f8df68fb7e7a7c7ac10af46da9
Avonexile <arnevanommen@outlook.com>
2018-02-13 13:41:15 +0100

    Shooting script

---
+++ Unity/Project Survival/Assets/Classes/Arne/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public Camera cam;
+
+	public KeyCode key;
+
+	//sets cursorstate
+	void Awake () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		Shoot();
+	}
+	public void Shoot () 
+	{
+		RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Input.GetKeyDown(key))
+		{
+			if (Physics.Raycast(ray, out hit)) 
+			{
+            	GameObject objectHit = hit.collider.gameObject; //what you hit
+				Destroy(objectHit);
+
+				Debug.Log(hit.collider.gameObject.name); //print name of hit target
+			}
+		} 
+	}
+}
